0dd613e40c41da775345fb659b5fe8a85e1bd03f
kunal saini <sainikunal605@gmail.com>
2025-11-24 08:52:19 +0530

    Implement inverted half pyramid with numbers

--- InvertedHalfPyramidWithNumber.java
+++ InvertedHalfPyramidWithNumber.java
@@ -10,7 +10,12 @@ public class InvertedHalfPyramidWithNumber {
     System.out.print("Enter the  number: ");
     int num=st.nextInt();
 
-    for(int i=)
+    for(int i=num; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            System.out.print(j);
+        }
+        System.out.println("");
+    }
 
 
 
